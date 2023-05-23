bdaffe2b25a51616d5af644a94d30424a2b0a49b
Brendan McGuire <brendan@bren.app>
2023-05-23 18:20:15 -0400

    Switch to vite build system

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "blorange-100": "#a29bfe",
+        "blorange-200": "#bc92f4",
+        "blorange-300": "#d28ae7",
+        "blorange-400": "#e481d8",
+        "blorange-500": "#f379c6",
+        "blorange-600": "#fd73b2",
+        "blorange-700": "#ff6f9d",
+        "blorange-800": "#ff6f88",
+        "blorange-900": "#ff7173",
+        "blorange-1000": "#ff765e",
+      },
+    },
+  },
+  plugins: [],
+};
