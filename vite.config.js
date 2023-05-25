0fd6d72170923ea32c07ed3167c11bb38851d842
Brendan McGuire <brendan@bren.app>
2023-05-24 22:23:01 -0400

    Support light mode, and minor fixes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "guides/index.html"),
+        guides: resolve(__dirname, "guides/index.html"),
       },
     },
   },
